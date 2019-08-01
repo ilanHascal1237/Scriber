2e35a8f33238b3cc31d53c788449f686054964e3
Kevin Choi <kevin.choi@yale.edu>
2019-08-01 19:23:31 -0400

    Commit for merge

--- App.js
+++ App.js
@@ -1,124 +1,19 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Audio } from "expo-av";
-import * as Permissions from "expo-permissions";
-import * as FileSystem from "expo-file-system";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { LoginScreen, LoginUserScreen, LoginHostScreen } from "./constants";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.recording = null;
+// export default function App() {
+//   return <View></View>;
+// }
 
-    this.state = {
-      isRecording: false,
-      isFetching: false
-    };
-  }
+const Navigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    LoginUser: LoginUserScreen,
+    LoginHost: LoginHostScreen
+  },
+  { initialRouteName: SCREENS.LOGIN }
+);
 
-  // onRecordPressed = () => {
-  //   if (this.state.isRecording) {
-  //     this._stopRecordingAndEnablePlayback();
-  //   } else {
-  //     this._stopPlaybackAndBeginRecording();
-  //   }
-  // };
-
-  startRecording = async () => {
-    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    if (status !== "granted") return;
-
-    this.setState({ isRecording: true });
-
-    const recordingOptions = {
-      // android not currently in use, but parameters are required
-      android: {
-        extension: ".m4a",
-        outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
-        audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
-        sampleRate: 44100,
-        numberOfChannels: 2,
-        bitRate: 128000
-      },
-      ios: {
-        extension: ".wav",
-        audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
-        sampleRate: 44100,
-        numberOfChannels: 1,
-        bitRate: 128000,
-        linearPCMBitDepth: 16,
-        linearPCMIsBigEndian: false,
-        linearPCMIsFloat: false
-      }
-    };
-
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: true,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      playThroughEarpieceAndroid: true,
-      staysActiveInBackground: false
-    });
-
-    const recording = Audio.Recording();
-
-    try {
-      await recording.prepareToRecordAsync(recordingOptions);
-      await recording.startAsync();
-    } catch (error) {
-      console.log(error);
-      this.stopRecording();
-    }
-
-    this.recording = recording;
-    console.log(this.recording.getURI());
-    this.getTranscription();
-  };
-
-  getTranscription = async () => {
-    this.setState({ isFetching: true });
-    try {
-      const info = await FileSystem.getInfoAsync(this.recording.getURI());
-      console.log(`FILE INFO: ${JSON.stringify(info)}`);
-      const uri = info.uri;
-      const formData = new FormData();
-      formData.append("file", {
-        uri,
-        type: "audio/x-wav",
-        // could be anything
-        name: "speech2text"
-      });
-      const response = await fetch(config.CLOUD_FUNCTION_URL, {
-        method: "POST",
-        body: formData
-      });
-      const data = await response.json();
-      this.setState({ query: data.transcript });
-    } catch (error) {
-      console.log("There was an error", error);
-      this.stopRecording();
-      this.resetRecording();
-    }
-    this.setState({ isFetching: false });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.startRecording}>
-          <Text>Tap to record</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+export default createAppContainer(Navigator);
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const morgan = require('morgan');
 
 app.use(morgan('tiny'));
-io.on('connection', socket => {
-	socket.emit('hi', 'YEET');
-});
+
+// io.on('connection', async socket => {
+// 	socket.emit('hi', 'YEET');
+
+// });
 
 const port = process.env.PORT || 3000;
 http.listen(port, function() {
