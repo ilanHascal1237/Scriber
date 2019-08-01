b4156bed293f6b964cc455f1f45c45cd41a3f231
Kevin Choi <kevin.choi@yale.edu>
2019-08-01 15:37:25 -0400

    Initial server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const morgan = require('morgan');
+
+app.use(morgan('tiny'));
+io.on('connection', socket => {
+	socket.on();
+});
+
+const port = process.env.PORT || 3000;
+http.listen(port, function() {
+	console.log('Listening on %s', port);
+});
