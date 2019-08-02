5dbd2f93b34e96e7053f7db96dc2d00ebf70105d
Kevin Choi <kevin.choi@yale.edu>
2019-08-01 22:22:41 -0400

    Add new message handler

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
 const morgan = require('morgan');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 app.use(morgan('tiny'));
 
-// const getRoom = socket => {
-// 	const rooms = Object.keys(socket.rooms);
-// 	if (rooms[0])
-// };
+const getRoom = socket => {
+	const rooms = Object.keys(socket.rooms);
+	if (rooms.length > 1) {
+		return rooms[0] === socket.id ? rooms[1] : rooms[0];
+	} else {
+		return null;
+	}
+};
 
 io.on('connection', socket => {
 	socket.on('makeRoom', roomId => {
@@ -18,25 +21,30 @@ io.on('connection', socket => {
 		if (room && room.length) {
 			socket.emit('dupRoom');
 		} else {
-			socket.join(roomId);
+            socket.join(roomId);
 		}
 	});
 
 	socket.on('joinRoom', roomId => {
 		const room = io.sockets.adapter.rooms[roomId];
 		if (room && room.length) {
-			socket.join(roomId);
+			const prevRoom = getRoom(socket);
+			if (prevRoom) {
+				socket.leave(prevRoom);
+			}
+            socket.join(roomId);
 		} else {
 			socket.emit('noRoom');
 		}
 	});
 
-	// socket.on('newMsg', msg => {
-	//     socket.rooms.keys()
-	// });
+	socket.on('newMsg', msg => {
+		const currRoom = getRoom(socket);
+		io.to(currRoom).emit('newMsg', msg);
+    });
 });
 
 const port = process.env.PORT || 3000;
-http.listen(port, function() {
+server.listen(port, function() {
 	console.log('Listening on %s', port);
 });
